f1296173b4bffb4280a89331e5b5de2a70012852
Rimjhim <rimjhimsaxena535@gmail.com>
2025-08-28 21:51:12 +0530

    day27

---
+++ LC26.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+#include <iostream>
+using namespace std;
+
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+        int i = 0;
+        int j = 1;
+
+        while(j < nums.size()){
+            if(nums[i] != nums[j]){
+                i++;
+
+                nums[i]=nums[j];
+
+            }
+            j++;
+        }
+        return i+1;
+    }
+};
