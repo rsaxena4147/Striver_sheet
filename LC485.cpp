f8c508c20346395632b1cc2ce16025923c9b3ef5
Rimjhim <rimjhimsaxena535@gmail.com>
2025-08-30 22:47:08 +0530

    day28

---
+++ LC485.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+#include <iostream>
+using namespace std;
+class Solution {
+    public int findMaxConsecutiveOnes(int[] nums) {
+          int count=0;
+        int ans = 0;
+        for(int i = 0 ; i < nums.length; i++){
+            if(nums[i]==1){
+                count++;
+                ans = Math.max(count , ans);
+               
+            }else{
+                count =0 ;
+            }
+        }
+        return ans;
+    }
+}
